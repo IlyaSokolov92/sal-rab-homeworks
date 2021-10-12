d17c24b6501ad1c508a284e29d6386502a32d77e
ilyaSokolov92 <ilya_sokolov92@mail.ru>
2021-10-12 22:03:32 +0400

    task_3 done

--- task_3/solution.js
+++ task_3/solution.js
@@ -9,6 +9,17 @@ function renderCartItem(item) {
     // title - наименование
     // price - цена
     // count - количество
+    let itemCountText = '';
+    itemCountText = itemCountText + product.count;
+    itemCountText = itemCountText + " × ";
+    itemCountText = itemCountText + product.price;
+    itemCountText = itemCountText + " ₽ = ";
+    let sum = product.count * product.price;
+    itemCountText = itemCountText + sum;
+    itemCountText = itemCountText + " ₽";
+
+
+
 
     // сформируйте строку itemCountText вида:
     // количество × цена ₽ = стоимость ₽
